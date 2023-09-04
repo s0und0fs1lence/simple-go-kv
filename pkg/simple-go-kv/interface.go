3f6683ddf9bad90ee9cd3a7ac4763a69b8b691a0
s0und0fs1lence <andrei@jfrogmedia.com>
2023-09-04 13:52:12 +0000

    removed unused functions

--- pkg/simple-go-kv/interface.go
+++ pkg/simple-go-kv/interface.go
@@ -6,9 +6,7 @@ type SimpleKV interface {
 	Has(key string) bool
 	Set(key string, value any, ttl *int) error
 	Delete(key string)
-	Deserialize(input []byte, output interface{}) error
 	Load(filename *string) error
 	Save(filename *string) error
-	TruncateDatabase()
 	GetEntryCount() uint32
 }
